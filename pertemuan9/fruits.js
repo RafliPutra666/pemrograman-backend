16620834c5e32432f1c7ca73a649e0f4c35fe28e
RafliPutra666 <88810105+RafliPutra666@users.noreply.github.com>
2025-01-01 15:30:14 +0700

    Delete pertemuan9 directory

--- pertemuan9/fruits.js
+++
@@ -1,7 +0,0 @@
-// TODO 1: 
-// - Buat array yg berisi data buah
-// - Refactor variable ke ES6 Variable.
-var fruits = ["Jeruk", "Apel", "Durian"];
-
-// TODO 2: Export variable fruits
-module.exports = fruits;
