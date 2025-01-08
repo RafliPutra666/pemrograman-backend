d4c163c52be1ebf2c70213772de8984ef0edd342
Rafli Putra <rafliputra1227@gmail.com>
2025-01-08 23:07:30 +0700

    praktikum12

--- praktikum12/controllers/StudentController.js
+++ praktikum12/controllers/StudentController.js
@@ -11,43 +11,7 @@ class StudentController {
     });
   }
 
-  // Menampilkan satu data student berdasarkan id
-  async show(req, res) {
-    const { id } = req.params;
-    const student = await Student.find(id);
-    res.json({
-      message: `Menampilkan student dengan id ${id}`,
-      data: student,
-    });
-  }
 
-  // Menambahkan data student baru
-  async store(req, res) {
-    const student = await Student.create(req.body);
-    res.json({
-      message: "Menambahkan data student",
-      data: student,
-    });
-  }
-
-  // Memperbarui data student
-  async update(req, res) {
-    const { id } = req.params;
-    const student = await Student.update(id, req.body);
-    res.json({
-      message: `Memperbarui data student dengan id ${id}`,
-      data: student,
-    });
-  }
-
-  // Menghapus data student
-  async destroy(req, res) {
-    const { id } = req.params;
-    const result = await Student.delete(id);
-    res.json({
-      message: result.message,
-    });
-  }
 }
 
 module.exports = new StudentController();
--- praktikum12/routes/api.js
+++ praktikum12/routes/api.js
@@ -7,9 +7,5 @@ const router = express.Router();
 
 // Define routes
 router.get("/students", StudentController.index);
-router.get("/students/:id", StudentController.show); 
-router.post("/students", StudentController.store);
-router.put("/students/:id", StudentController.update); 
-router.delete("/students/:id", StudentController.destroy);
 
 module.exports = router;
